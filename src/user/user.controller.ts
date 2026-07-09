c187a1aa439bda096379200d3109eee76bbdf0d9
edenWerke <edenw6292@gmail.com>
2026-07-09 11:08:32 +0300

    add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
@@ -17,5 +17,12 @@ export class UserController {
     }
 
     return users;
+  }
+    @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return {
+      id,
+      name: 'Eden',
+    };
   }
 }
