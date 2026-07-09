57f3b127a18df2b9d4b8e830db740de07f060ec1
edenWerke <edenw6292@gmail.com>
2026-07-09 10:28:40 +0300

    puh

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
-import { Controller ,Get} from '@nestjs/common';
+import { Controller ,Get, Query} from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
 
 @Get()
-getUser(){
+getUser(@Query('name')name:string){
     return [
-        {id:1,name:'Eden'}
+        {id:1,name:name||'Eden'}
     ]
 }
 
