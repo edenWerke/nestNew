3e271803964025e97636c426621e4ff487afd277
edenWerke <edenw6292@gmail.com>
2026-07-14 18:50:13 +0300

    add

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+interface User{
+    id:number;
+    name:string;
+    email:string;
+}
+
+
+
+
 @Injectable()
-export class UserService {}
+export class UserService {
+    private users:User[]=[
+        {id:1,name:'eden ',email:'eden@2024.com'}
+    ]
+}
