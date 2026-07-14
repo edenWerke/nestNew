df9504804aafeee141987182f6afa7756586a46b
edenWerke <edenw6292@gmail.com>
2026-07-14 18:59:13 +0300

    add

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
-interface User{
-    id:number;
-    name:string;
-    email:string;
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
-
-
-
 
 @Injectable()
 export class UserService {
-    private users:User[]=[
-        {id:1,name:'eden ',email:'eden@2024.com'},
-        {id:2, name:'natty', email:'natty@2024.com'}
-    ]
+  private users: User[] = [
+    { id: 1, name: 'eden', email: 'eden@2024.com' },
+    { id: 2, name: 'natty', email: 'natty@2024.com' },
+  ];
+
+  findAllUsers(name: string = ''): User[] {
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
 }
